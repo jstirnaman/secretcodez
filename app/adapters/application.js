540a1158b9316afd23858898e5e6edbe610bedc3
Jason Stirnaman <stirnamanj@gmail.com>
2016-10-01 19:11:04 -0500

    Replace authentication functionality of custom auth with ember-simple-auth.

--- app/adapters/application.js
+++ app/adapters/application.js
@@ -1,16 +1,9 @@
-import Ember from 'ember';
 import DS from 'ember-data';
+import DataAdapterMixin from 'ember-simple-auth/mixins/data-adapter-mixin';
 
 export default DS.JSONAPIAdapter.extend({});
 
-export default DS.RESTAdapter.extend({
+export default DS.RESTAdapter.extend(DataAdapterMixin, {
   namespace: 'api',
-
-  authManager: Ember.inject.service(),
-
-  headers: Ember.computed('authManager.accessToken', function() {
-    return {
-      "Authorization": `Bearer ${this.get("authManager.accessToken")}`
-    };
-  })
+  authorizer: 'authorizer:application'
 });
